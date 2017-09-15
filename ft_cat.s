a4ba5eadcda89f11ec21e845f60224a0e23c18cf
Kingh-T <mohlala.tumelo@gmail.com>
2017-09-15 20:24:47 +0200

    test

--- ft_cat.s
+++ ft_cat.s
@@ -3,4 +3,20 @@ global _ft_cat
 section .text
 
 _ft_cat:
+    pop rbx
+    pop rbx
+    pop rbx
+    mov rax,5
+    mov rcx,0
+    int 80h
+    mov rax,3
+    mov rbx,rax
+    mov rcx,buffer
+    mov rdx,
+
+section .data
+    buffsize dw 43
+
+section .bss
+    buffer resb 
     
